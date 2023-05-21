05acc387498aa167dcb9558d9e3a3e24d71e1140
Alvaro <alvarogoncalves923@gmail.com>
2023-05-21 11:34:48 -0300

    mensagem

--- 06 - importacao e Exportacao/aula.js
+++ 06 - importacao e Exportacao/aula.js
@@ -1,23 +1,2 @@
-// 3) Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".
-const pessoa = {
-    nome: 'alvaro', 
-    sobrenome: 'goncalves'
-};
 
-
-
-const nomes = ['Lucas', 'Vinicius', 'Pedro', 'Vitoria', 'Emanuela', 'Valdileia', 'Victor', 'Pablo'];
-var letraComparacao = "V";
-
-for (let i = 0; i < pessoa.length; i++) {
-    var primeiraLetra = pessoa[i].charAt(0);
-
-
-  if (primeiraLetra === letraComparacao) {
-        console.log(pessoa[i] + " começa com a letra " + letraComparacao);
-
-    } else {
-    }
-
-}
 
